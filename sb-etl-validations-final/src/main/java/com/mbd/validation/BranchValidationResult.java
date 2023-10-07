74f95607814eb55c9ec7cf07240fde26538ed4b5
Madhubala3916 <73657310+Madhubala3916@users.noreply.github.com>
2023-10-07 13:12:03 +0530

    All validations done

---
+++ sb-etl-validations-final/src/main/java/com/mbd/validation/BranchValidationResult.java
@@ -0,0 +1,28 @@
+package com.mbd.validation;
+
+
+
+import com.mbd.model.Branch;
+import jakarta.validation.ConstraintViolation;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class BranchValidationResult {
+    private final List<Branch> validBranches;
+    private final Map<Branch, Set<ConstraintViolation<Branch>>> erroneousBranches;
+
+    public BranchValidationResult(List<Branch> validBranches, Map<Branch, Set<ConstraintViolation<Branch>>> erroneousBranches) {
+        this.validBranches = validBranches;
+        this.erroneousBranches = erroneousBranches;
+    }
+
+    public List<Branch> getValidBranches() {
+        return validBranches;
+    }
+
+    public Map<Branch, Set<ConstraintViolation<Branch>>> getErroneousBranches() {
+        return erroneousBranches;
+    }
+}
